6f8a5d4b84449500d2a1c3978d637afb5e6ff157
summer-alice <stigma+github@disroot.org>
2021-03-29 11:06:15 +1000

    [Gtk] Add AccessibleTextAttributeEvent class

--- org.eclipse.swt.gtk.linux.x86/src/org/eclipse/swt/accessibility/all.d
+++ org.eclipse.swt.gtk.linux.x86/src/org/eclipse/swt/accessibility/all.d
@@ -1,7 +1,5 @@
 module org.eclipse.swt.accessibility.all;
 
-import java.lang.all;
-
 public import org.eclipse.swt.accessibility.ACC;
 public import org.eclipse.swt.accessibility.Accessible;
 public import org.eclipse.swt.accessibility.AccessibleAdapter;
@@ -16,5 +14,6 @@ public import org.eclipse.swt.accessibility.AccessibleFactory;
 public import org.eclipse.swt.accessibility.AccessibleListener;
 public import org.eclipse.swt.accessibility.AccessibleObject;
 public import org.eclipse.swt.accessibility.AccessibleTextAdapter;
+public import org.eclipse.swt.accessibility.AccessibleTextAttributeEvent;
 public import org.eclipse.swt.accessibility.AccessibleTextEvent;
 public import org.eclipse.swt.accessibility.AccessibleTextListener;
