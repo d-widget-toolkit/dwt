40a2fb39a587cbc1ca1c1206933cba2e4eb85323
summer-alice <stigma+github@disroot.org>
2023-08-29 11:34:38 +1000

    Update SWTError to 4.7
    Changed from PlatformException to Error since it better indicates the
    type of Throwable, which is one that typically shouldn't occur in a
    program, but high-reliability programs can still catch.

--- org.eclipse.swt/Eclipse SWT/common/org/eclipse/swt/SWTError.d
+++ org.eclipse.swt/Eclipse SWT/common/org/eclipse/swt/SWTError.d
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2000, 2008 IBM Corporation and others.
+ * Copyright (c) 2000, 2011 IBM Corporation and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -44,7 +44,7 @@ import org.eclipse.swt.SWT;
  * @see <a href="http://www.eclipse.org/swt/">Sample code and further information</a>
  */
 
-public class SWTError : PlatformException {
+public class SWTError : Error {
     /**
      * The SWT error code, one of SWT.ERROR_*.
      */
@@ -54,12 +54,12 @@ public class SWTError : PlatformException {
      * The underlying throwable that caused the problem,
      * or null if this information is not available.
      */
-    public Throwable throwable( Throwable e ){
-        this.next = e;
+    public Throwable throwable() {
         return this.next;
     }
-    public Throwable throwable(){
-        return this.next;
+
+    public void throwable(Throwable t) {
+        this.next = t;
     }
 
     //static const long serialVersionUID = 3833467327105808433L;
